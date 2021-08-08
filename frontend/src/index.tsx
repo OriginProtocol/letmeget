5eb4a8c2071d902039e2493f101e9390eef352fc
Mike Shultz <shultzm@gmail.com>
2021-08-07 22:34:02 -0600

    add https redirect

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react"
+import { render } from "react-dom"
 
-import App from './App'
+import App from "./App"
 
-render(<App />, document.getElementById('root'))
+const SSL_ENFORCE_HOSTNAMES = [
+  "letmeget.io",
+  "www.letmeget.io",
+  "letmeget.app",
+  "www.letmeget.app",
+  "letmeget.org",
+  "www.letmeget.org",
+]
+if (
+  location.protocol !== "https" &&
+  SSL_ENFORCE_HOSTNAMES.includes(location.hostname)
+) {
+  location.href = `https://letmeget.io${location.pathname}`
+}
+
+render(<App />, document.getElementById("root"))
