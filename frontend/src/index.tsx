9b7eb8e4d9c8d39a0bd01b6c55cd4fa194629941
Mike Shultz <shultzm@gmail.com>
2021-08-11 21:23:26 -0600

    fix Location.protocol comparison

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -12,7 +12,7 @@ const SSL_ENFORCE_HOSTNAMES = [
   "www.letmeget.org",
 ]
 if (
-  location.protocol !== "https" &&
+  location.protocol !== "https:" &&
   SSL_ENFORCE_HOSTNAMES.includes(location.hostname)
 ) {
   location.href = `https://letmeget.io${location.pathname}`
